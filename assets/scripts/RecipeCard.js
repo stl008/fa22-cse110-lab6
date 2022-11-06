c5826fbe78c8cb892de6f0253f26acf9b0671dad
stl008 <stl008@ucsd.edu>
2022-11-06 14:49:00 -0800

    Trying something with append for the shadow dom

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -91,7 +91,7 @@ class RecipeCard extends HTMLElement {
       font-size: 12px;
     }`;
       // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-      shadow.appendChild(article);
+      shadow.append(article);
       shadow.appendChild(style);
   }
 
