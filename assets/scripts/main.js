f21224c895222485aea56c996d62ec60dc235903
stl008 <stl008@ucsd.edu>
2022-11-06 12:28:42 -0800

    first draft expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  var arr = [];
+  let recipes = JSON.parse(localStorage.getItem("recipes"));
+  if (recipes == null)
+  {
+    return arr;
+  }
+  return recipes;
 }
 
 /**
@@ -35,10 +42,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  var mainRef = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for(const recipe in recipes)
+  {
+    let card = new RecipeCard();
+    card.data = recipe;
+    mainRef.append(card);
+  }
 }
 
 /**
